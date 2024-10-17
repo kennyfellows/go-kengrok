d8434c607f962ab1cde35a7c41934cb22f2d8310
Kenneth Fellows <kennyfellows@gmail.com>
2024-10-17 19:13:02 -0400

    grpc server basic

---
+++ cmd/tunnel-manager/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	pb "go-kengrok/proto/tunnel-manager"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+  pb.UnimplementedTunnelManagerServer
+}
+
+func (s *server) CreateTunnel (ctx context.Context, req *pb.CreateTunnelRequest)(*pb.CreateTunnelResponse, error) {
+  log.Printf("Received: %v", req.GetDestinationPort() )
+
+  return &pb.CreateTunnelResponse{ TunnelId: "tunnel-" + req.GetDestinationPort() }, nil
+}
+
+func main() {
+  lis, err := net.Listen( "tcp", ":50051" )
+
+  if err != nil {
+    log.Fatalf( "Failed to listen: %v", err )
+  }
+
+  s := grpc.NewServer()
+  pb.RegisterTunnelManagerServer( s, &server{} )
+  log.Printf( "server listening at %v", lis.Addr() )
+
+  if err := s.Serve( lis ); err != nil {
+    log.Fatalf( "Failed to serve: %v", err )
+  }
+}
+
+
