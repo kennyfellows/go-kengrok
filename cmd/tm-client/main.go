e3faddf4c1f856b7e8177580ebead58a5607813a
Kenneth Fellows <kennyfellows@gmail.com>
2024-10-17 22:36:21 -0400

    Basic grpc server and client

---
+++ cmd/tm-client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	pb "go-kengrok/proto/tunnel-manager"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+  conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+  if err != nil {
+    fmt.Println( "Error connecting to grpc server ", err.Error() )
+  }
+
+  defer conn.Close()
+
+	c := pb.NewTunnelManagerClient(conn)
+
+  ctx, cancel := context.WithTimeout( context.Background(), time.Second )
+
+	defer cancel()
+
+	r, err := c.CreateTunnel( ctx, &pb.CreateTunnelRequest{DestinationPort: "4444"} )
+
+  if err != nil {
+		log.Fatalf("could not create tunnel: %v", err)
+	}
+
+  log.Printf("Tunnel created: %s", r.GetTunnelId())
+}
